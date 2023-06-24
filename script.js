0251d199dc2d8004c2c9202b1d88542206298d82
praneeth-ayla <praneethayla@gmail.com>
2023-06-24 18:40:45 +0530

    etch-a-sketch with basic UI

--- script.js
+++ script.js
@@ -1,47 +1,182 @@
 const gridContainer = document.querySelector("#container");
-let gridSize = parseInt(prompt("Enter the grid size: "));
+let SelectedColor = 'black';
+let gridSizeInput = document.querySelector("#gridSize");
+gridSize = 20;
+let gridCells;
+let isMouseDown = false;
 
-let gridCount = 0
 
-while (gridCount<(gridSize*gridSize)) {
+
+// making grid 
+function createGrid() {
+  // removes existing grids before we change to other grid sizes
+  while (gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.firstChild);
+  }
+
+  gridSize = parseInt(gridSizeInput.value);
+
+  gridContainer.style.gridTemplateColumns = `repeat(${gridSize},1fr)`;
+  gridContainer.style.gridTemplateRows = `repeat(${gridSize},1fr)`;
+
+  let gridCount = 0
+  while (gridCount<(gridSize*gridSize)) {
     const gridDivs =   document.createElement("div");
-    gridDivs.setAttribute('class',"divs");
+    gridDivs.classList.add("divs", "grid-border");
     gridContainer.appendChild(gridDivs);
     gridCount++
 }
 
-gridContainer.style.gridTemplateColumns = `repeat(${gridSize},1fr)`;
-gridContainer.style.gridTemplateRows = `repeat(${gridSize},1fr)`;
-// gridDivs.style.border = "solid black";
+gridCells = document.querySelectorAll(".divs");
 
-let gridCells = document.querySelectorAll(".divs");
-var isMouseDown = false;
+
+//etching the grids
 
 gridCells.forEach(cell =>{
  
     cell.addEventListener("mousedown", () => {
         isMouseDown = true;
         cell.classList.add('hovered');
-        cell.style.backgroundColor = 'black';
+        cell.style.backgroundColor = `${SelectedColor}`;
     });
 
     cell.addEventListener("mouseup", ()=>{
         isMouseDown = false;
-        cell.style.backgroundColor = 'black';
+        cell.style.backgroundColor = `${SelectedColor}`;
         cell.classList.remove("hovered");
     });
 
     cell.addEventListener("mouseover", () =>{
         if (isMouseDown) {
             cell.classList.add("hovered");
-            cell.style.backgroundColor = 'black';
+            cell.style.backgroundColor = `${SelectedColor}` ;
 
         }
-    })
+    });
+});
+}
 
+
+// changing the color
+var radioButtons = document.querySelectorAll('input[type="radio"][name="colors"]');
+
+radioButtons.forEach(function(radioButton) {
+  radioButton.addEventListener('change', function() {
+    SelectedColor = this.value;
+  });
 });
 
 
-// let hovers = document.querySelectorAll(".hovered");
 
-// hovers.style.backgroundColor = "red";
+// clear button
+function clearGrid() {
+    gridCells.forEach(cell => {
+      cell.style.backgroundColor = "Whitesmoke"; // Set the background color to an empty string
+    });
+  }
+
+createGrid();
+
+
+
+
+const changeGridSizeBtn = document.querySelector("#changeGridSizeBtn");
+changeGridSizeBtn.addEventListener("click", () => {
+  createGrid(); // Update the grid size and recreate the grid
+});
+
+
+let clearBtn = document.querySelector("#clearBtn");
+clearBtn.addEventListener('click',()=>{
+    clearGrid();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+// const gridContainer = document.querySelector("#container");
+// let SelectedColor = 'black';
+// let gridSize = 20;
+// let gridCells;
+// let isMouseDown = false;
+
+// // Making grid 
+// function createGrid() {
+//   // Remove existing grids before we change to other grid sizes
+//   while (gridContainer.firstChild) {
+//     gridContainer.removeChild(gridContainer.firstChild);
+//   }
+
+//   gridSize = parseInt(document.querySelector("#gridSize").value);
+
+//   gridContainer.style.gridTemplateColumns = `repeat(${gridSize},1fr)`;
+//   gridContainer.style.gridTemplateRows = `repeat(${gridSize},1fr)`;
+
+//   let gridCount = 0;
+//   while (gridCount < (gridSize * gridSize)) {
+//     const gridDivs = document.createElement("div");
+//     gridDivs.classList.add("divs", "grid-border");
+//     gridContainer.appendChild(gridDivs);
+//     gridCount++;
+//   }
+
+//   gridCells = document.querySelectorAll(".divs");
+
+//   // Etching the grids
+//   gridCells.forEach(cell => {
+//     cell.addEventListener("mousedown", () => {
+//       isMouseDown = true;
+//       cell.classList.add('hovered');
+//       cell.style.backgroundColor = SelectedColor;
+//     });
+
+//     cell.addEventListener("mouseup", () => {
+//       isMouseDown = false;
+//       cell.style.backgroundColor = SelectedColor;
+//       cell.classList.remove("hovered");
+//     });
+
+//     cell.addEventListener("mouseover", () => {
+//       if (isMouseDown) {
+//         cell.classList.add("hovered");
+//         cell.style.backgroundColor = SelectedColor;
+//       }
+//     });
+//   });
+// }
+
+// // Changing the color
+// var radioButtons = document.querySelectorAll('input[type="radio"][name="colors"]');
+
+// radioButtons.forEach(function(radioButton) {
+//   radioButton.addEventListener('change', function() {
+//     SelectedColor = this.value;
+//   });
+// });
+
+// // Clear button
+// function clearGrid() {
+//   gridCells.forEach(cell => {
+//     cell.style.backgroundColor = "whitesmoke";
+//   });
+// }
+
+// createGrid();
+
+// const changeGridSizeBtn = document.querySelector("#changeGridSizeBtn");
+// changeGridSizeBtn.addEventListener("click", () => {
+//   createGrid(); // Update the grid size and recreate the grid
+// });
+
+// let clearBtn = document.querySelector("#clearBtn");
+// clearBtn.addEventListener('click', () => {
+//   clearGrid();
+// });
